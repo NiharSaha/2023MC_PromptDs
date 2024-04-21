ba7f6fa691c2424cfbf6d48de8bb6c4f2076b7c7
Nihar Saha <56932008+NiharSaha@users.noreply.github.com>
2024-04-21 20:35:09 +0530

    Update Ds_Prompt_phikkpi_pthat0_cfg.py

--- Ds_Prompt_phikkpi_pthat0_cfg.py
+++ Ds_Prompt_phikkpi_pthat0_cfg.py
@@ -3,7 +3,7 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
 from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
 
-generator = cms.EDFilter("Pythia8GeneratorFilter",
+_generator = cms.EDFilter("Pythia8GeneratorFilter",
                          pythiaPylistVerbosity = cms.untracked.int32(0),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
                          comEnergy = cms.double(5360.0),
@@ -52,6 +52,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                      )
 
 from GeneratorInterface.Core.ExternalGeneratorFilter import ExternalGeneratorFilter
+generator = ExternalGeneratorFilter(_generator)
 generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 
 partonfilter = cms.EDFilter("PythiaFilter",
